4a48a27b50f6389ae8f2e65e39ba073e32fa3f8e
Eric Berry <eric@Eric-Berrys-MacBook.local>
2009-10-02 10:35:34 -0600

    first commit

---
+++ init.rb
@@ -0,0 +1,2 @@
+require File.dirname(__FILE__) + '/lib/tweet_watcher'
+ActionView::Base.send(:include, TweetWatcherHelper)
---
+++ lib/tweet_watcher.rb
@@ -0,0 +1,38 @@
+module TweetWatcherHelper
+  def tweet_watcher(word, options={})
+    options[:title]      = ''     if options[:title].blank?
+    options[:subject]    = ''     if options[:subject].blank?
+    options[:width]      = 200    if options[:width].blank?
+    options[:height]     = 400    if options[:height].blank?
+    options[:text_color] = '#39c' if options[:text_color].blank?
+    options[:link_color] = '#444' if options[:link_color].blank?
+    ret = <<-EOF
+    <div class="twtr-widget" id="twtr-search-widget"></div>
+    <script src="http://widgets.twimg.com/j/1/widget.js"></script>
+    <link href="http://widgets.twimg.com/j/1/widget.css" type="text/css" rel="stylesheet">
+    <script>
+      new TWTR.Widget({
+        search:'teachmetocode',
+        id:'twtr-search-widget',
+        loop:false,
+        title:'#{options[:title]}',
+        subject:'#{options[:subject]}',
+        width:#{options[:width]},
+        height:#{options[:height]},
+        theme:{
+          shell:{
+            background:'transparent',
+            color:'#{options[:link_color]}'
+          },
+          tweets:{
+            background:'transparent',
+            color:'#{options[:text_color]}',
+            links:'#{options[:link_color]}'
+          }
+        }
+      }).render().start();
+    </script>
+    EOF
+    return ret
+  end
+end
